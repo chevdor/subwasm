b062c94cf59b7745c68c25f49ce8bfc77e151258
Wilfried Kopp <wilfried@parity.io>
2021-05-07 16:02:48 +0200

    Fix the demos

--- scripts/demos/run-all.sh
+++ scripts/demos/run-all.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 . env.sh
 
-mkdir -p casts svg
+mkdir -p casts gif
 
 demos=( \
     demo-get \
@@ -14,7 +14,11 @@ for demo in ${demos[@]}; do
     echo Recoding demos:
     echo - $demo
     asciinema rec --overwrite casts/$demo.cast -c ./$demo.sh
-    termtosvg render -t window_frame_js casts/$demo.cast svg/$demo.svg -D 3000
+    
+    # SVG do not play in the readme in gitlab...
+    # termtosvg render -t window_frame_js casts/$demo.cast svg/$demo.svg -D 3000
+
+    asciicast2gif casts/$demo.cast gif/$demo.gif
 done
 
 rm -f *.wasm
