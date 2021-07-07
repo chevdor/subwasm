fe23586350e5a3636b10e920acffa492692dbcca
TeraBot <chevdor@users.noreply.github.com>
2021-07-07 15:32:38 +0000

    build: new homebrew formula for v0.12.1

--- Formula/subwasm.rb
+++ Formula/subwasm.rb
@@ -1,9 +1,9 @@
 class Subwasm < Formula
   desc "A command line utility written in Rust download, inspect and compare Substrate based chains WASM Runtimes"
   homepage "https://github.com/chevdor/subwasm"
-  url "https://github.com/chevdor/subwasm/releases/download/v0.12.0/subwasm_macos_v0.12.0.tar.gz"
-  sha256 "c9c0d85cf91d1b3f0ceef1b66513ac552cc9fab48aef76c5f23fb61ae6dbf48f"
-  version "0.12.0"
+  url "https://github.com/chevdor/subwasm/releases/download/v0.12.1/subwasm_macos_v0.12.1.tar.gz"
+  sha256 "a34068aba5a4d75aad1d435e542ff184285a9fc03f3f7538a7be07ca2560db0b"
+  version "0.12.1"
 
   def install
     bin.install "subwasm"
