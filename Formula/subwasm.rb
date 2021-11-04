dea259315054e4270186c62b0b047c46d23a1c84
Chevdor <chevdor@gmail.com>
2021-11-04 12:17:57 +0100

    Fix homebrew hash

--- Formula/subwasm.rb
+++ Formula/subwasm.rb
@@ -2,7 +2,7 @@ class Subwasm < Formula
   desc "A command line utility written in Rust download, inspect and compare Substrate based chains WASM Runtimes"
   homepage "https://github.com/chevdor/subwasm"
   url "https://github.com/chevdor/subwasm/releases/download/v0.16.1/subwasm_macos_v0.16.1.tar.gz"
-  sha256 "a16821c02737dd83ceb08c742d6a0d2cbfd368a690d9936e236dd52efc82e060"
+  sha256 "b9036e5229d7ba738200fa165b7c874ac57310746e02f6f7e2b1c8a9b8910fe5"
   version "0.16.1"
 
   def install
