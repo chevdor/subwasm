edf0c06811bd608bf43af9c3b48917ec7d356089
Chevdor <chevdor@gmail.com>
2022-05-09 14:29:36 +0200

    WIP

--- libs/wasm-testbed/src/lib.rs
+++ libs/wasm-testbed/src/lib.rs
@@ -3,8 +3,11 @@ mod logger_mock;
 
 pub use error::{Result, WasmTestbedError};
 use frame_metadata::{RuntimeMetadata, RuntimeMetadataPrefixed};
-use sc_executor::{CallInWasm, WasmExecutionMethod, WasmExecutor};
+use sc_executor::Externalities;
+use sc_executor::{RuntimeVersion, WasmExecutionMethod, WasmExecutor};
 use scale::Decode;
+// use sp_core::traits::CodeExecutor;
+use sc_executor_common::runtime_blob::RuntimeBlob;
 use sp_core::Hasher;
 use sp_runtime::traits::BlakeTwo256;
 use sp_version::RuntimeVersion as SubstrateRuntimeVersion;
@@ -128,22 +131,20 @@ impl WasmTestBed {
 	/// as we have no blocks, storage, etc...
 	fn call(wasm: &[u8], method: &str, call_data: &[u8]) -> Result<Vec<u8>> {
 		let mut ext = sp_state_machine::BasicExternalities::default();
-		let mut host_functions = sp_io::SubstrateHostFunctions::host_functions();
-		host_functions.push(&logger_mock::LoggerMock);
+		// let mut ext = sp_state_machine::BasicExternalities::new_empty();
+		// let mut host_functions = sp_io::SubstrateHostFunctions::host_functions();
+		// host_functions.push(&logger_mock::LoggerMock);
 
-		let executor = WasmExecutor::new(
-			WasmExecutionMethod::Interpreted,
-			// At least 12 for Polkadot V12/V13.
-			// Substrate V14 requires 34.
-			// Polkadot V14 requires 20.
-			Some(64),
-			host_functions,
-			8,
-			None,
-		);
+		// At least 12 for Polkadot V12/V13.
+		// Substrate V14 requires 34.
+		// Polkadot V14 requires 20.
+		let executor = WasmExecutor::new(WasmExecutionMethod::Interpreted, Some(64), 8, None, 2);
 
+		let runtime_blob = RuntimeBlob::new(wasm).unwrap();
 		executor
-			.call_in_wasm(wasm, None, method, call_data, &mut ext, sp_core::traits::MissingHostFunctions::Allow)
+			.uncached_call(runtime_blob, &mut ext, true, method, call_data)
+			// .call(None, &wasm, method, call_data)
+			// .call_in_wasm(wasm, None, method, call_data, &mut ext, sp_core::traits::MissingHostFunctions::Allow)
 			.map_err(|_| WasmTestbedError::Calling(method.to_string()))
 	}
 
