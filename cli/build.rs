07c6bffbf55cde417ae1e9312b3dca9ed402061f
Chevdor <chevdor@gmail.com>
2023-02-09 15:10:58 +0100

    Swap the --version flag for a new one providing both git hash and build date

---
+++ cli/build.rs
@@ -0,0 +1,51 @@
+use std::{borrow::Cow, process::Command};
+// use chrono;
+
+fn main() {
+	generate_cargo_keys();
+}
+
+/// Generate the `cargo:` key output
+pub fn generate_cargo_keys() {
+	let commit = if let Ok(hash) = std::env::var("SUBWASM_CLI_GIT_COMMIT_HASH") {
+		Cow::from(hash.trim().to_owned())
+	} else {
+		// We deliberately set the length here to `11` to ensure that
+		// the emitted hash is always of the same length; otherwise
+		// it can (and will!) vary between different build environments.
+		match Command::new("git").args(["rev-parse", "--short=11", "HEAD"]).output() {
+			Ok(o) if o.status.success() => {
+				let tmsp = String::from_utf8_lossy(&o.stdout).trim().to_owned();
+				Cow::from(tmsp)
+			}
+			Ok(o) => {
+				let status = o.status;
+				println!("cargo:warning=Git command failed with status: {status}");
+				Cow::from("unknown")
+			}
+			Err(err) => {
+				println!("cargo:warning=Failed to execute git command: {err}");
+				Cow::from("unknown")
+			}
+		}
+	};
+
+	let build_date = match Command::new("date").args(["+%Y-%m-%dT%H:%M:%S%z"]).output() {
+		Ok(o) if o.status.success() => {
+			let sha = String::from_utf8_lossy(&o.stdout).trim().to_owned();
+			Cow::from(sha)
+		}
+		Ok(o) => {
+			let status = o.status;
+			println!("cargo:warning=Failed fetching the date timestamp: {status}");
+			Cow::from("unknown")
+		}
+		Err(err) => {
+			println!("cargo:warning=Failed fetching the datge: {err}");
+			Cow::from("unknown")
+		}
+	};
+
+	println!("cargo:rustc-env=SUBWASM_CLI_BUILD_DATE={build_date}");
+	println!("cargo:rustc-env=SUBWASM_CLI_GIT_COMMIT_HASH={commit}");
+}
