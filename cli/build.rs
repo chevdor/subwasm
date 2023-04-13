1c91e990e8a751fc095337e15a3f8559567eba7b
Chevdor <chevdor@gmail.com>
2023-04-13 10:26:16 +0200

    Cleanup

--- cli/build.rs
+++ cli/build.rs
@@ -1,11 +1,10 @@
 use std::{borrow::Cow, process::Command};
-// use chrono;
 
 fn main() {
 	generate_cargo_keys();
 }
 
-/// Generate the `cargo:` key output
+/// Generate the cargo keys
 pub fn generate_cargo_keys() {
 	generate_cargo_key_git();
 	generate_cargo_key_build_date();
@@ -35,9 +34,6 @@ pub fn generate_cargo_key_git() {
 	let commit = if let Ok(hash) = std::env::var("SUBWASM_CLI_GIT_COMMIT_HASH") {
 		Cow::from(hash.trim().to_owned())
 	} else {
-		// We deliberately set the length here to `11` to ensure that
-		// the emitted hash is always of the same length; otherwise
-		// it can (and will!) vary between different build environments.
 		match Command::new("git").args(["rev-parse", "--short=11", "HEAD"]).output() {
 			Ok(o) if o.status.success() => {
 				let tmsp = String::from_utf8_lossy(&o.stdout).trim().to_owned();
