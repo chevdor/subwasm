ab929354746cfd29f877bf4666a0852c72e628d4
Chevdor <chevdor@gmail.com>
2023-05-05 17:46:32 +0200

    Add some chain urls

--- lib/src/chain_urls.rs
+++ lib/src/chain_urls.rs
@@ -1,22 +1,31 @@
 use std::str::FromStr;
 use wasm_loader::NodeEndpoint;
 
+// TODO: use subrpc !
 pub fn get_chain_urls(name: &str) -> Option<Vec<NodeEndpoint>> {
 	match name {
 		"polkadot" | "dot" => Some(vec![
 			"wss://rpc.polkadot.io:443",
+			"wss://polkadot-rpc-tn.dwellir.com:443",
+			"wss://polkadot-rpc.dwellir.com:443",
 			// "wss://polkadot.api.onfinality.io:443/public-ws",
 			// "wss://polkadot.elara.patract.io",
 		]),
 		"kusama" | "ksm" => Some(vec![
 			"wss://kusama-rpc.polkadot.io:443",
-			"wss://kusama.api.onfinality.io:443/public-ws",
-			// "wss://kusama.elara.patract.io",
+			"wss://kusama-rpc-tn.dwellir.com:443",
+			"wss://kusama-rpc.dwellir.com:443",
+			"wss://kusama-rpc.polkadot.io:443",
+			"wss://kusama-try-runtime-node.parity-chains.parity.io:443",
+			"wss://kusama.public.curie.radiumblock.co:443/ws",
+			"wss://rpc.dotters.network:443/kusama",
+			"wss://rpc.ibp.network:443/kusama",
+			// "wss://kusama.api.onfinality.io:443/public-ws",
 		]),
 		"westend" | "wnd" => Some(vec![
 			"wss://westend-rpc.polkadot.io:443",
-			// "wss://westend.api.onfinality.io:443/public-ws",
-			// "wss://westend.elara.patract.io",
+			"wss://westend-try-runtime-node.parity-chains.parity.io:443", // "wss://westend.api.onfinality.io:443/public-ws",
+			                                                              // "wss://westend.elara.patract.io",
 		]),
 		"rococo" => Some(vec![
 			"wss://rococo-rpc.polkadot.io:443",
