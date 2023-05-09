chevdor/subwasm

commit cfed86871503e4e4f9db4427e5b79ae4b310a15e

feat: add option to output metadata to a file (#61)
* add option to output metadata to a file in either scale or json encoding

* refactor metadata printing

* cargo clippy --fix and cargo fmt

* don't panic on broken pipe

* update cargo lock

* improve error handling by properly installing eyre and replacing lots of panics and unwraps

* Minor fixes

* Add doc and user goodie

* cargo +nightly clippy --fix

* fix typo

* Generate doc

---------

Co-authored-by: Chevdor <chevdor@users.noreply.github.com>
Co-authored-by: Chevdor <chevdor@gmail.com>

cli/src/main.rs
@@ -1,5 +1,7 @@
 mod opts;
 
+use std::io::Write;
+
 use clap::{crate_name, crate_version, Parser};
 // use color_eyre::owo_colors::OwoColorize;
 use env_logger::Env;
@@ -20,6 +22,8 @@ macro_rules! noquiet {
 fn main() -> color_eyre::Result<()> {
 	env_logger::Builder::from_env(Env::default().default_filter_or("none")).init();
 	let opts: Opts = Opts::parse();
+	color_eyre::install()?;
+
 	noquiet!(opts, println!("Running {} v{}", crate_name!(), crate_version!()));
 
 	match opts.subcmd {
@@ -57,13 +61,43 @@ fn main() -> color_eyre::Result<()> {
 			info!("⏱️  Loading WASM from {:?}", &source);
 			let subwasm = Subwasm::new(&source);
 
-			if let Some(filter) = meta_opts.module {
-				subwasm.display_module(filter);
-			} else if opts.json {
-				subwasm.display_metadata_json()
-			} else {
-				subwasm.display_modules_list()
+			let mut fmt: OutputFormat = meta_opts.format.unwrap_or_else(|| "human".into()).into();
+			if opts.json {
+				eprintln!("--json is DEPRECATED, use --format=json instead");
+				fmt = OutputFormat::Json;
 			}
+
+			let mut output = meta_opts.output;
+			if let Some(out) = &output {
+				if out.is_empty() || out == "auto" {
+					match fmt {
+						OutputFormat::Human => output = Some("metadata.txt".into()),
+						OutputFormat::Json => output = Some("metadata.json".into()),
+						OutputFormat::Scale => output = Some("metadata.scale".into()),
+						OutputFormat::HexScale => output = Some("metadata.hex".into()),
+						OutputFormat::JsonScale => output = Some("metadata.jscale".into()),
+					}
+				}
+			}
+
+			let mut out: Box<dyn Write> = if let Some(output) = &output {
+				Box::new(std::fs::File::create(output)?)
+			} else {
+				Box::new(std::io::stdout())
+			};
+
+			match subwasm.write_metadata(fmt, meta_opts.module, &mut out) {
+				Ok(_) => Ok(()),
+				Err(e) => {
+					if let Some(e) = e.root_cause().downcast_ref::<std::io::Error>() {
+						if e.kind() == std::io::ErrorKind::BrokenPipe {
+							log::debug!("ignoring broken pipe error: {:?}", e);
+							return Ok(());
+						}
+					}
+					Err(e)
+				}
+			}?
 		}
 
 		SubCommand::Diff(diff_opts) => {
@@ -77,11 +111,11 @@ fn main() -> color_eyre::Result<()> {
 		}
 
 		SubCommand::Compress(copts) => {
-			compress(copts.input, copts.output).unwrap();
+			compress(copts.input, copts.output)?;
 		}
 
 		SubCommand::Decompress(dopts) => {
-			decompress(dopts.input, dopts.output).unwrap();
+			decompress(dopts.input, dopts.output)?;
 		}
 	};
 

lib/Cargo.toml
@@ -18,7 +18,6 @@ repository = "https://github.com/chevdor/subwasm"
 version = "0.18.0"
 
 [dependencies]
-calm_io = "0.1"
 color-eyre = "0.6"
 frame-metadata = { version = "15", package = "frame-metadata", features = [
     "v12",
@@ -39,3 +38,4 @@ substrate-differ = { version = "0.18.0", path = "../libs/substrate-differ" }
 wasm-loader = { version = "0.18.0", path = "../libs/wasm-loader" }
 wasm-testbed = { version = "0.18.0", path = "../libs/wasm-testbed" }
 sp-version = { tag = "monthly-2023-01", git = "https://github.com/paritytech/substrate" }
+hex = "0.4"

lib/src/lib.rs
@@ -1,3 +1,4 @@
+use color_eyre::eyre::eyre;
 use std::path::Path;
 use std::{fs::File, path::PathBuf};
 use std::{io::prelude::*, str::FromStr};
@@ -17,6 +18,7 @@ mod subwasm;
 mod types;
 pub use chain_info::*;
 use log::{debug, info};
+pub use metadata_wrapper::OutputFormat;
 pub use runtime_info::*;
 pub use subwasm::*;
 pub use types::*;
@@ -67,7 +69,7 @@ pub fn download_runtime(url: &str, block_ref: Option<BlockRef>, output: Option<P
 	let url = match url {
 		url if url.starts_with("ws") => NodeEndpoint::WebSocket(url.to_string()),
 		url if url.starts_with("http") => NodeEndpoint::Http(url.to_string()),
-		_ => panic!("The url should either start with http or ws"),
+		_ => return Err(eyre!("The url should either start with http or ws")),
 	};
 
 	let reference = OnchainBlock { endpoint: url, block_ref };
@@ -129,41 +131,41 @@ pub fn diff(src_a: Source, src_b: Source) {
 
 /// Compress a given runtime into a new file. You cannot compress
 /// a runtime that is already compressed.
-pub fn compress(input: PathBuf, output: PathBuf) -> Result<(), String> {
-	let wasm = WasmLoader::load_from_source(&Source::File(input)).unwrap();
+pub fn compress(input: PathBuf, output: PathBuf) -> color_eyre::Result<()> {
+	let wasm = WasmLoader::load_from_source(&Source::File(input))?;
 
 	if wasm.compression().compressed() {
-		return Err("The input is already compressed".into());
+		return Err(eyre!("The input is already compressed"));
 	}
 
-	let bytes_compressed = Compression::compress(wasm.original_bytes()).unwrap();
+	let bytes_compressed = Compression::compress(wasm.original_bytes()).map_err(|e| eyre!(e))?;
 
 	debug!("original   = {:?}", wasm.original_bytes().len());
 	debug!("compressed = {:?}", bytes_compressed.len());
 	info!("Saving compressed runtime to {:?}", output);
 
-	let mut buffer = File::create(output).unwrap();
-	buffer.write_all(&bytes_compressed.to_vec()).unwrap();
+	let mut buffer = File::create(output)?;
+	buffer.write_all(&bytes_compressed.to_vec())?;
 
 	Ok(())
 }
 
 /// Decompress a given runtime file. It is fine decompressing an already
 /// decompressed runtime, you will just get the same.
-pub fn decompress(input: PathBuf, output: PathBuf) -> Result<(), String> {
-	let wasm = WasmLoader::load_from_source(&Source::File(input)).unwrap();
+pub fn decompress(input: PathBuf, output: PathBuf) -> color_eyre::Result<()> {
+	let wasm = WasmLoader::load_from_source(&Source::File(input))?;
 
 	let bytes_decompressed = match wasm.compression().compressed() {
 		false => wasm.original_bytes().clone(),
-		true => Compression::decompress(wasm.original_bytes()).unwrap(),
+		true => Compression::decompress(wasm.original_bytes()).map_err(|e| eyre!(e))?,
 	};
 
 	debug!("original     = {:?}", wasm.original_bytes().len());
 	debug!("decompressed = {:?}", bytes_decompressed.len());
 
 	info!("Saving decompressed runtime to {:?}", output);
-	let mut buffer = File::create(output).unwrap();
-	buffer.write_all(&bytes_decompressed.to_vec()).unwrap();
+	let mut buffer = File::create(output)?;
+	buffer.write_all(&bytes_decompressed.to_vec())?;
 
 	Ok(())
 }

commit 5d77a9bbe561eb2bf45793c5618693ddf976dc25

WIP: Fix regression introduced in #61

cli/src/main.rs
@@ -1,12 +1,11 @@
 mod opts;
 
-use std::{env, io::Write};
-
 use clap::{crate_name, crate_version, Parser};
 use env_logger::Env;
 use log::info;
 use opts::*;
 use serde_json::json;
+use std::{env, io::Write};
 use subwasmlib::*;
 use text_style::{AnsiColor, StyledStr};
 
@@ -102,7 +101,7 @@ fn main() -> color_eyre::Result<()> {
 
 			match diff_opts.method {
 				DiffMethod::Reduced => {
-					let diff_result = reduced_diff(src_a, src_b);
+					let diff_result = reduced_diff(src_a, src_b).expect("Reduced diff failed");
 					if opts.json {
 						let s = serde_json::to_string_pretty(&diff_result).unwrap();
 						println!("{s}");
@@ -121,8 +120,7 @@ fn main() -> color_eyre::Result<()> {
 						println!("{diff_result}");
 						text_style::termion::render(std::io::stdout(), &warning).expect("Could not render line");
 					}
-				}
-				DiffMethod::Summary => todo!(),
+				} // DiffMethod::Summary => todo!(),
 			}
 		}
 
@@ -165,9 +163,10 @@ fn main() -> color_eyre::Result<()> {
 			let subwasm = Subwasm::new(&source);
 
 			if let Some(pallet) = sr_opts.pallet {
-				subwasm.display_reduced_pallet(&pallet, opts.json)
+				// todo: fix previous branches and DO return an anyhow:Result<()> instead of ignoring the output
+				let _ = subwasm.display_reduced_pallet(&pallet, opts.json);
 			} else {
-				subwasm.display_reduced_runtime(opts.json)
+				let _ = subwasm.display_reduced_runtime(opts.json);
 			}
 		}
 	};

lib/Cargo.toml
@@ -40,3 +40,5 @@ wasm-loader = { version = "1.0.0-alpha6", path = "../libs/wasm-loader" }
 wasm-testbed = { version = "1.0.0-alpha6", path = "../libs/wasm-testbed" }
 sp-version = { tag = "monthly-2023-05", git = "https://github.com/paritytech/substrate" }
 hex = "0.4"
+anyhow = "1.0"
+calm_io = "0.1"

lib/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(clippy::derive_partial_eq_without_eq)]
+use anyhow::Result;
 use color_eyre::eyre::eyre;
 // use std::path::Path;
 // use std::{fs::File, path::PathBuf};
@@ -17,6 +18,7 @@ mod metadata_wrapper;
 mod runtime_info;
 mod subwasm;
 mod types;
+mod utils;
 
 use log::{debug, info};
 pub use metadata_wrapper::OutputFormat;
@@ -119,21 +121,17 @@ pub fn download_runtime(url: &str, block_ref: Option<BlockRef>, output: Option<P
 }
 
 //todo: return Result and no expect
-pub fn reduced_diff(src_a: Source, src_b: Source) -> ReducedDiffResult {
+pub fn reduced_diff(src_a: Source, src_b: Source) -> Result<ReducedDiffResult> {
 	log::debug!("REDUCED: Loading WASM runtimes:");
 	log::info!("  🅰️  {:?}", src_a);
-	let runtime_a = WasmTestBed::new(&src_a).expect("Can only diff if the 2 runtimes can load");
+	let runtime_a = WasmTestBed::new(&src_a)?;
 	log::info!("  🅱️  {:?}", src_b);
-	let runtime_b = WasmTestBed::new(&src_b).expect("Can only diff if the 2 runtimes can load");
+	let runtime_b = WasmTestBed::new(&src_b)?;
 
-	// let differ = ReducedDiffer::new(, runtime_b.metadata());
 	let ra = ReducedRuntime::from(runtime_a.metadata());
 	let rb = ReducedRuntime::from(runtime_b.metadata());
-	// let changes = ReducedDiffer::compare(&ra, &rb);
-	// let da = DiffAnalyzer::new(&ra, &rb, &changes);
-	// let compatible = da.compatible();
 
-	ReducedDiffResult::new(ra, rb)
+	Ok(ReducedDiffResult::new(ra, rb))
 }
 
 /// Compress a given runtime into a new file. You cannot compress

lib/src/metadata_wrapper.rs
@@ -5,7 +5,7 @@ use frame_metadata::RuntimeMetadata;
 use log::debug;
 use scale_info::scale::Encode;
 
-use crate::{convert::convert, write_module, write_v14_meta};
+use crate::{convert::convert, utils::print_big_output_safe, write_module, write_v14_meta};
 
 /// The output format for the metadata
 #[derive(Debug, Clone, Copy)]
@@ -53,14 +53,17 @@ impl<'a> MetadataWrapper<'a> {
 				if filter.is_some() {
 					return Err(eyre!("Cannot filter metadata in json format"));
 				} else {
-					serde_json::to_writer_pretty(out, &self.0)?;
+					let serialized = serde_json::to_string_pretty(&self.0)?;
+					let _ = print_big_output_safe(&serialized);
 				}
 			}
 			OutputFormat::Scale => {
 				if filter.is_some() {
 					return Err(eyre!("Cannot filter metadata in scale format"));
 				} else {
-					out.write_all(&self.0.encode())?;
+					let scale = &self.0.encode();
+					// let _ = print_big_output_safe(&scale.to_ascii_lowercase());
+					out.write_all(scale)?;
 				}
 			}
 			OutputFormat::HexScale => {
@@ -68,7 +71,8 @@ impl<'a> MetadataWrapper<'a> {
 					return Err(eyre!("Cannot filter metadata in hex+scale format"));
 				} else {
 					let encoded = self.0.encode();
-					write!(out, "0x{}", hex::encode(encoded))?;
+					let hexscale = format!("0x{}", hex::encode(encoded));
+					let _ = print_big_output_safe(&hexscale);
 				}
 			}
 			OutputFormat::JsonScale => {
@@ -78,7 +82,7 @@ impl<'a> MetadataWrapper<'a> {
 					let encoded = self.0.encode();
 					let hex = format!("0x{}", hex::encode(encoded));
 					let json = serde_json::to_string_pretty(&serde_json::json!({ "result": hex }))?;
-					write!(out, "{json}")?;
+					let _ = print_big_output_safe(&json);
 				}
 			}
 		}

lib/src/subwasm.rs
@@ -2,9 +2,11 @@ use std::io::Write;
 
 use crate::{
 	metadata_wrapper::{self, MetadataWrapper},
+	utils::print_big_output_safe,
 	RuntimeInfo,
 };
 
+use anyhow::bail;
 use substrate_differ::differs::reduced::reduced_runtime::ReducedRuntime;
 use wasm_loader::Source;
 use wasm_testbed::{WasmTestBed, WasmTestbedError};
@@ -41,26 +43,6 @@ impl Subwasm {
 		&self.runtime_info
 	}
 
-	// pub fn display_module(&self, filter: String) {
-	// TODO: clean up
-	// pub fn display_infos(&self) -> color_eyre::Result<()> {
-	// 	let metadata = self.testbed.runtime_metadata_prefixed();
-
-	// 	match &metadata.1 {
-	// 		RuntimeMetadata::V12(_v12) => {
-	// 			println!("Detected Substrate Runtime V12");
-	// 		}
-	// 		RuntimeMetadata::V13(_v13) => {
-	// 			println!("Detected Substrate Runtime V13");
-	// 		}
-	// 		RuntimeMetadata::V14(_v14) => {
-	// 			println!("Detected Substrate Runtime V14");
-	// 		}
-	// 		_ => return Err(eyre::eyre!("Unsupported metadata version")),
-	// 	};
-	// 	Ok(())
-	// }
-
 	pub fn write_metadata<O: Write>(
 		&self,
 		fmt: metadata_wrapper::OutputFormat,
@@ -72,80 +54,31 @@ impl Subwasm {
 		wrapper.write(fmt, filter, out)
 	}
 
-	// pub fn display_modules_list(&self) {
-	// 	let metadata = self.testbed.runtime_metadata_prefixed();
-	// 	let wrapper = MetadataWrapper(&metadata.1);
-	// 	wrapper.display_modules_list();
-	// }
-
-	pub fn display_reduced_runtime(&self, json: bool) {
+	pub fn display_reduced_runtime(&self, json: bool) -> anyhow::Result<()> {
 		let reduced_runtime: ReducedRuntime = self.testbed.metadata().into();
+
 		if json {
-			println!("{}", serde_json::to_string_pretty(&reduced_runtime).expect("Failed encoding reduced runtime"));
+			let serialized = serde_json::to_string_pretty(&reduced_runtime)?;
+			print_big_output_safe(&serialized)
 		} else {
-			println!("{reduced_runtime}");
+			print_big_output_safe(&reduced_runtime.to_string())
 		}
 	}
 
-	pub fn display_reduced_pallet(&self, pallet: &str, json: bool) {
+	pub fn display_reduced_pallet(&self, pallet: &str, json: bool) -> anyhow::Result<()> {
 		let reduced_runtime: ReducedRuntime = self.testbed.metadata().into();
 		let pallet_maybe = reduced_runtime.get_pallet_by_name(pallet);
 
 		if let Some(reduced_pallet) = pallet_maybe {
 			if json {
-				println!("{}", serde_json::to_string_pretty(&reduced_pallet).expect("Failed encoding reduced runtime"));
+				let serialized = serde_json::to_string_pretty(&reduced_pallet)?;
+
+				print_big_output_safe(&serialized)
 			} else {
-				println!("{reduced_pallet}");
+				print_big_output_safe(&reduced_pallet.to_string())
 			}
 		} else {
-			println!("Pallet '{pallet}' not found.");
+			bail!("Pallet '{pallet}' not found.")
 		}
 	}
-
-	// /// Display the metadata as json
-	// pub fn display_metadata_json(&self) {
-	// 	let pallet_filter: Option<String> = None;
-	// 	let metadata = self.testbed.metadata();
-
-	// 	let serialized = if let Some(ref pallet) = pallet_filter {
-	// 		match metadata {
-	// 			RuntimeMetadata::V12(v12) => {
-	// 				let modules = convert(&v12.modules);
-
-	// 				let pallet_metadata = modules
-	// 					.iter()
-	// 					.find(|module| module.name == DecodeDifferent::Decoded(pallet.into()))
-	// 					.expect("pallet not found in metadata");
-	// 				serde_json::to_string_pretty(&pallet_metadata)
-	// 			}
-	// 			// RuntimeMetadata::V13(v13) => {
-	// 			// 	let pallet = v13
-	// 			// 		.modules
-	// 			// 		.iter()
-	// 			// 		.find(|m| &m.name == pallet)
-	// 			// 		.ok_or_else(|| eyre::eyre!("pallet not found in metadata"))?;
-	// 			// 	serde_json::to_string_pretty(&pallet)?
-	// 			// }
-	// 			_ => panic!("Unsupported metadata version"),
-	// 		}
-	// 	} else {
-	// 		serde_json::to_string_pretty(&metadata)
-	// 	};
-
-	// 	// The following fails if piped to another command that truncates the output.
-	// 	// Typical use case here is: subwasm meta | head
-	// 	// The failure is due to https://github.com/rust-lang/rust/issues/46016
-	// 	// TODO: Once the above is fixed, we can remove the dependency on calm_io
-	// 	// println!("{}", serialized);
-
-	// 	let serialized = serialized.unwrap();
-	// 	let _ = match stdoutln!("{}", serialized) {
-	// 		Ok(_) => Ok(()),
-	// 		Err(e) => match e.kind() {
-	// 			std::io::ErrorKind::BrokenPipe => Ok(()),
-	// 			_ => Err(e),
-	// 		},
-	// 	};
-	// 	wrapper.write(fmt, filter, out)
-	// }
 }

lib/src/utils.rs
@@ -1,0 +1,21 @@
+use anyhow::bail;
+
+// TODO: Once the lined issue is fixed, we can remove the dependency on calm_io
+/// There is a bug caused by printing big output to stdout.
+///
+/// We need to take extra precautions due to the following bug:
+/// https://github.com/rust-lang/rust/issues/46016
+pub fn print_big_output_safe(s: &str) -> anyhow::Result<()> {
+	// The following fails if piped to another command that truncates the output.
+	// println!("{}", s);
+	// Typical use cases here are:
+	// 	- subwasm meta --chain westend --json | head
+	// 	- subwasm meta --chain westend --json | less
+	match calm_io::stdoutln!("{}", s) {
+		Ok(_) => Ok(()),
+		Err(e) => match e.kind() {
+			std::io::ErrorKind::BrokenPipe => Ok(()),
+			_ => bail!(e),
+		},
+	}
+}

libs/substrate-differ/src/differs/diff_method.rs
@@ -2,8 +2,8 @@ use std::str::FromStr;
 
 #[derive(Debug, Clone)]
 pub enum DiffMethod {
-	/// Summary
-	Summary,
+	// /// Summary
+	/// Summary,
 
 	/// The runtimes are reduced first and the reduced runtimes are compared
 	Reduced,
@@ -14,7 +14,7 @@ impl FromStr for DiffMethod {
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
 		match s.to_lowercase().as_str() {
-			"summary" => Ok(DiffMethod::Summary),
+			// "summary" => Ok(DiffMethod::Summary),
 			"reduced" | "partial" => Ok(DiffMethod::Reduced),
 			_ => Err(format!("Cannot convert '{s}' to a known DiffMethod")),
 		}

libs/wasm-loader/src/source.rs
@@ -9,8 +9,13 @@ use crate::{NodeEndpoint, OnchainBlock};
 /// The source of the wasm. It can come from the local file system (`File`) or from a chain (`Chain`).
 #[derive(Debug, Clone, PartialEq)]
 pub enum Source {
+	/// A file on your local filesystem
 	File(PathBuf),
+
+	/// A remote endpoint we can connect to
 	Chain(OnchainBlock),
+	// /// A chain alias such as "westend"
+	//Alias(String),
 }
 
 impl Display for Source {
@@ -85,4 +90,13 @@ mod tests {
 			assert!(Source::from_str(url) == Ok(Source::File(PathBuf::from(url))));
 		}
 	}
+
+	// #[test]
+	// fn it_converts_from_alias() {
+	// 	let urls = vec!["foo", "bar"];
+
+	// 	for url in urls {
+	// 		assert!(Source::from_str(url) == Ok(Source::Alias(url.into())));
+	// 	}
+	// }
 }

libs/wasm-testbed/Cargo.toml
@@ -40,3 +40,4 @@ sp-wasm-interface = { tag = "monthly-2023-05", git = "https://github.com/parityt
 substrate-runtime-proposal-hash = { version = "1.0.0-alpha6", path = "../substrate-runtime-proposal-hash" }
 wasm-loader = { version = "1.0.0-alpha6", path = "../wasm-loader" }
 sp-version = { tag = "monthly-2023-05", git = "https://github.com/paritytech/substrate" }
+anyhow = "1.0"

libs/wasm-testbed/src/error.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{error::Error, fmt};
 
 pub type Result<T> = std::result::Result<T, WasmTestbedError>;
 
@@ -9,6 +9,8 @@ pub enum WasmTestbedError {
 	Calling(String),
 	Decoding(Vec<u8>),
 }
+
+impl Error for WasmTestbedError {}
 
 impl fmt::Display for WasmTestbedError {
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {

cli/tests/compress.rs
@@ -1,0 +1,33 @@
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod cli_compress {
+		use assert_cmd::Command;
+
+		#[test]
+		fn it_does_basic_compress_decompress() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "compressed.wasm"]).assert();
+			assert.success().code(0);
+
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			cmd.args(["decompress", "compressed.wasm", "decompressed.wasm"]).assert().success().code(0);
+
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			cmd.args(["compress", "decompressed.wasm", "new_compressed.wasm"]).assert().success().code(0);
+		}
+
+		#[test]
+		fn it_does_decompress_on_already() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "compressed.wasm"]).assert();
+			assert.success().code(0);
+
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			cmd.args(["decompress", "compressed.wasm", "decompressed.wasm"]).assert().success().code(0);
+
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			cmd.args(["decompress", "decompressed.wasm", "new_decompressed.wasm"]).assert().success().code(0);
+		}
+	}
+}

cli/tests/diff.rs
@@ -1,0 +1,18 @@
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod diff {
+		use assert_cmd::Command;
+
+		#[test]
+		fn it_shows_metadata() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "runtime.wasm"]).assert();
+			assert.success().code(0);
+
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["meta", "runtime.wasm"]).assert();
+			assert.success().code(0);
+		}
+	}
+}

cli/tests/get.rs
@@ -1,0 +1,24 @@
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod get {
+		use assert_cmd::Command;
+		use std::path::Path;
+		#[test]
+		fn it_gets_a_runtime() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+			let assert = cmd.args(["get", "--output", "runtime.wasm", "wss://rpc.polkadot.io:443"]).assert();
+			assert.success().code(0);
+			assert!(Path::new("runtime.wasm").exists());
+		}
+
+		#[test]
+		fn it_fails_on_bad_chain() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+			let assert = cmd.args(["get", "--chain", "foobar"]).assert();
+			assert.failure().code(101);
+		}
+	}
+}

cli/tests/info.rs
@@ -1,0 +1,13 @@
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod info {
+		use assert_cmd::Command;
+		#[test]
+		fn it_fails_without_source() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.arg("info tcp://foo.bar").assert();
+			assert.failure().code(2);
+		}
+	}
+}

cli/tests/meta.rs
@@ -1,0 +1,48 @@
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod meta {
+		use assert_cmd::Command as AssertCommand;
+		use std::process::{Command, Stdio};
+
+		#[test]
+		fn it_shows_metadata() {
+			let mut cmd = AssertCommand::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "runtime.wasm"]).assert();
+			assert.success().code(0);
+
+			let mut cmd = AssertCommand::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["meta", "runtime.wasm"]).assert();
+			assert.success().code(0);
+		}
+
+		// todo: test/fix following
+		// c run -- meta --chain wnd -f json | head        # OK
+		// c run -- meta --chain wnd -f scale | head       # FAIL
+		// c run -- meta --chain wnd -f hex+scale | head    # FIXED
+		// c run -- meta --chain wnd -f json+scale | head   # FIXED
+		// #[test]
+		// fn it_shows_when_piped() {
+		// 	// let mut cmd = AssertCommand::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+		// 	// let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "runtime.wasm"]).assert();
+		// 	// assert.success().code(0);
+
+		// 	let subwasm_process = Command::new(env!("CARGO_PKG_NAME"))
+		// 		.args(["meta", "wss://rpc.polkadot.io:443", "-f", "json"])
+		// 		.stdout(Stdio::piped())
+		// 		.spawn()
+		// 		.unwrap();
+		// 	let head_process = Command::new("head")
+		// 		.stdin(Stdio::from(subwasm_process.stdout.unwrap())) // Pipe through.
+		// 		.stdout(Stdio::piped())
+		// 		.spawn()
+		// 		.unwrap();
+		// 	let output = head_process.wait_with_output().unwrap();
+
+		// 	let s = String::from_utf8_lossy(&output.stdout);
+		// 	println!("s = {:?}", s);
+		// 	println!("s = {:?}", s.len());
+		// 	assert!(s.len() > 100)
+		// }
+	}
+}

cli/tests/misc.rs
@@ -1,0 +1,15 @@
+#[cfg(test)]
+mod cli_tests {
+
+	#[cfg(test)]
+	mod help {
+		use assert_cmd::Command;
+
+		#[test]
+		fn it_shows_help() {
+			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.arg("--help").assert();
+			assert.success().code(0);
+		}
+	}
+}

cli/tests/test.rs
@@ -1,95 +1,0 @@
-#[cfg(test)]
-mod cli_tests {
-
-	#[cfg(test)]
-	mod help {
-		use assert_cmd::Command;
-
-		#[test]
-		fn it_shows_help() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.arg("--help").assert();
-			assert.success().code(0);
-		}
-	}
-
-	#[cfg(test)]
-	mod info {
-		use assert_cmd::Command;
-		#[test]
-		fn it_fails_without_source() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.arg("info tcp://foo.bar").assert();
-			assert.failure().code(2);
-		}
-	}
-
-	#[cfg(test)]
-	mod get {
-		use assert_cmd::Command;
-		use std::path::Path;
-		#[test]
-		fn it_gets_a_runtime() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-
-			let assert = cmd.args(["get", "--output", "runtime.wasm", "wss://rpc.polkadot.io:443"]).assert();
-			assert.success().code(0);
-			assert!(Path::new("runtime.wasm").exists());
-		}
-
-		#[test]
-		fn it_fails_on_bad_chain() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-
-			let assert = cmd.args(["get", "--chain", "foobar"]).assert();
-			assert.failure().code(101);
-		}
-	}
-
-	#[cfg(test)]
-	mod meta {
-		use assert_cmd::Command;
-
-		#[test]
-		fn it_shows_metadata() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "runtime.wasm"]).assert();
-			assert.success().code(0);
-
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.args(["meta", "runtime.wasm"]).assert();
-			assert.success().code(0);
-		}
-	}
-
-	#[cfg(test)]
-	mod compress {
-		use assert_cmd::Command;
-
-		#[test]
-		fn it_does_basic_compress_decompress() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "compressed.wasm"]).assert();
-			assert.success().code(0);
-
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			cmd.args(["decompress", "compressed.wasm", "decompressed.wasm"]).assert().success().code(0);
-
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			cmd.args(["compress", "decompressed.wasm", "new_compressed.wasm"]).assert().success().code(0);
-		}
-
-		#[test]
-		fn it_does_decompress_on_already() {
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			let assert = cmd.args(["get", "wss://rpc.polkadot.io:443", "--output", "compressed.wasm"]).assert();
-			assert.success().code(0);
-
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			cmd.args(["decompress", "compressed.wasm", "decompressed.wasm"]).assert().success().code(0);
-
-			let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-			cmd.args(["decompress", "decompressed.wasm", "new_decompressed.wasm"]).assert().success().code(0);
-		}
-	}
-}