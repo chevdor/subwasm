2e621d5115aea4294bca3c0c985a262cddb00253
Chevdor <chevdor@gmail.com>
2023-05-15 12:30:03 +0200

    Revert "build: new homebrew formula for v1.0.0-alpha7"
    This reverts commit a0cb3b08582ee7699813c7fe908d167e7512f57a.

--- Formula/subwasm.rb
+++ Formula/subwasm.rb
@@ -1,9 +1,9 @@
 class Subwasm < Formula
   desc "A command line utility written in Rust download, inspect and compare Substrate based chains WASM Runtimes"
   homepage "https://github.com/chevdor/subwasm"
-  url "https://github.com/chevdor/subwasm/releases/download/v1.0.0-alpha7/subwasm_macos_v1.0.0-alpha7.tar.gz"
-  sha256 "732f1abec2ea7277ac1467e4f07060d22bdda00a7a09222cb5a9ba6f80e3f6e4"
-  version "1.0.0-alpha7"
+  url "https://github.com/chevdor/subwasm/releases/download/v0.19.1/subwasm_macos_v0.19.1.tar.gz"
+  sha256 "a23ce6b27f59f63dc6ae417f3a99f5ee788648e66a647dad94b3fe5f3ef760d8"
+  version "0.19.1"
 
   def install
     bin.install "subwasm"
