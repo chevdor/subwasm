071e70b40a88176fd7d34a6c50542ed710a87e24
Chevdor <chevdor@gmail.com>
2023-06-20 09:21:59 +0200

    Set version for new release

---
+++ cli/tests/show.rs
@@ -0,0 +1,17 @@
+mod test_utils;
+#[cfg(test)]
+mod cli_tests {
+	#[cfg(test)]
+	mod show {
+		use assert_cmd::Command as AssertCommand;
+
+		#[test]
+		fn it_shows_metadata() {
+			let test_wasm: &str = &crate::test_utils::ensure_local_wasm();
+
+			let mut cmd = AssertCommand::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+			let assert = cmd.args(["show", "--json"]).arg(test_wasm).assert();
+			assert.success().code(0);
+		}
+	}
+}
